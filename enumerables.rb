ea5d6091720da9c8013684e48ef3150f5c9f43d4
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-15 11:43:03 +0300

    Fix LInter Error

--- enumerables.rb
+++ enumerables.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 module MyEnumerable
   def all?
     @list.each { |num| return false unless yield(num) }
     true
   end
-def any?
-    @list.each  { |num| return true if yield(num) }
+
+  def any?
+    @list.each { |num| return true if yield(num) }
     false
   end
+
   def filter
     result = []
-    @list.each  { |num| result << num if yield(num) }
+    @list.each { |num| result << num if yield(num) }
     result
   end
 end
