de1127ce28395aa48135287c51db59ec227133fa
AMARACHI DIMKPA <amaradimkpa1@gmail.com>
2023-06-15 09:40:16 +0100

    Add filter and check the even.

--- enumerables.rb
+++ enumerables.rb
@@ -3,7 +3,15 @@ module MyEnumerable
     @list.each { |num| return false unless yield(num) }
     true
   end
-
+def any?
+    @list.each  { |num| return true if yield(num) }
+    false
+  end
+  def filter
+    result = []
+    @list.each  { |num| result << num if yield(num) }
+    result
+  end
 end
 
 class MyList
@@ -22,3 +30,6 @@ list = MyList.new([1, 2, 3, 4])
 
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
