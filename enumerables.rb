4a3f01f6a45d1f1bad33bc437bc534bad8896516
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-15 12:41:20 +0300

    Modularize Enum

--- enumerables.rb
+++ enumerables.rb
@@ -1,20 +1,4 @@
-module MyEnumerable
-  def all?
-    @list.each { |num| return false unless yield(num) }
-    true
-  end
-
-  def any?
-    @list.each { |num| return true if yield(num) }
-    false
-  end
-
-  def filter
-    result = []
-    @list.each { |num| result << num if yield(num) }
-    result
-  end
-end
+require_relative "myEnums"
 
 class MyList
   include MyEnumerable
